3933d709b4de13c8f709fd258ce11506afade4bd
krille <mks160615@gmail.com>
2026-07-17 09:22:06 +0900

    change structure

---
+++ src/A_Theory/B_TypeScript/TheComponents.js
@@ -0,0 +1,13 @@
+import B_Animation from "./B_Animation";
+import C_DarkMode from "./C_DarkMode";
+import D_Theme from "./D_Theme"
+
+const TheComponents = () => {
+    return(
+        <>
+            <D_Theme />
+        </>
+    )
+}
+
+export default TheComponents;
