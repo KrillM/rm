dbf600c503aaee18f096367578094b7112c95f70
krille <mks160615@gmail.com>
2026-08-04 14:41:47 +0900

    transform

--- src/F_Animation/App_A.tsx
+++ src/F_Animation/App_A.tsx
@@ -1,9 +1,9 @@
 import styled from "styled-components";
-import { motion, transform, useMotionValue, Variants, useTransform, useViewportScroll } from "framer-motion";
+import { motion, transform, Variants } from "framer-motion";
 import { useRef } from "react";
 
-const Wrapper = styled(motion.div)`
-    height: 200vh;
+const Wrapper = styled.div`
+    height: 100vh;
     width: 100%;
     display: flex;
     justify-content: center;
@@ -35,14 +35,6 @@ const Box4Variant = styled(motion.div)`
     display: grid;
     grid-template-columns: repeat(2, 1fr);
     background-color: rgba(255,255,255,0.2);
-    border-radius: 40px;
-    box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
-`;
-
-const Box4Gesture = styled(motion.div)`
-    width: 200px;
-    height: 200px;
-    background-color: rgba(255,255,255,1);
     border-radius: 40px;
     box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
@@ -101,22 +93,9 @@ const boxGesture = {
 
 export default function App(){
     const parentBoxRef = useRef<HTMLDivElement>(null);
-    const x = useMotionValue(0);
-    const scaleX = useTransform(x, [-800,0,800], [2,1,0]);
-    const rotateX = useTransform(x, [-800,800], [-360,360]);
-    const gradient = useTransform(
-        x,
-        [-800,800],
-        [
-            "linear-gradient(135deg, rgb(0, 210, 238), rgb(0, 83, 238))",
-            "linear-gradient(135deg, rgb(0, 238, 155), rgb(238, 178, 0))",
-        ]
-    )
-    const {scrollYProgress} = useViewportScroll();
-    const scale = useTransform(scrollYProgress, [0,1], [1,5]);
 
     return (
-        <Wrapper style={{background: gradient}}>
+        <Wrapper>
             {/* Basic */}
             {/* <Box 
                 transition={{type:"tween", stiffness: 10}} // 기본값: spring
@@ -139,8 +118,8 @@ export default function App(){
             </Box4Variant> */}
 
             {/* Gestures */}
-            {/* <ParentBox ref={parentBoxRef}>
-                <Box4Gesture 
+            <ParentBox ref={parentBoxRef}>
+                <Box 
                     drag // 이동 가능하나 제한이 없다
                     dragSnapToOrigin // 이동 후 마우스 클릭을 해체하면 원래 위치로 돌아온다
                     dragElastic={0.5} // dragElastic의 기본 값
@@ -150,19 +129,7 @@ export default function App(){
                     whileDrag="drag"
                     whileTap="click"
                 />
-            </ParentBox> */}
-
-            {/* Motion Value */}
-            {/* <Box 
-                style={{ x, scale: scaleX }}
-                drag="x" 
-                dragSnapToOrigin 
-            /> */}
-            <Box 
-                style={{ x, rotateX, scale }}
-                drag="x" 
-                dragSnapToOrigin 
-            />
+            </ParentBox>
         </Wrapper>
     );
 }
