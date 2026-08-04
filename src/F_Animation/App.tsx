68c7652263e41c04c26d8f4597d46de08c0fa858
krille <mks160615@gmail.com>
2026-08-04 20:23:00 +0900

    slider

--- src/F_Animation/App.tsx
+++ src/F_Animation/App.tsx
@@ -4,10 +4,11 @@ import { useState } from "react";
 
 const Wrapper = styled.div`
     height: 100vh;
-    width: 100%;
+    width: 100vw;
     display: flex;
     justify-content: center;
     align-items: center;
+    flex-direction: column;
 `;
 
 const Box = styled(motion.div)`
@@ -15,46 +16,74 @@ const Box = styled(motion.div)`
     height: 200px;
     background-color: rgba(255,255,255,1);
     border-radius: 40px;
-    position:absolute;
+    position: absolute;
     top: 100px;
+    display: flex;
+    justify-content:center;
+    align-items: center;
+    font-size: 28px;
     box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
-const boxVariants = {
-    initial: {
-        opacity:0,
-        scale:0,
+const BtnArea = styled.div`
+    display: flex;
+`
+
+const box = {
+    entry: (isBack: boolean) => ({
+        x: isBack ? -500 : 500,
+        opacity: 0,
+        scale: 0,
+    }),
+    center: {
+        x: 0,
+        opacity: 1,
+        scale: 1,
     },
-    visible: {
-        opacity:1,
-        scale:1,
-        rotateZ: 360
-    },
-    leaving: {
-        opacity:0,
-        y: 20
-    }
+    exit: (isBack: boolean) => ({
+        x: isBack ? 500 : -500,
+        opacity: 0,
+        scale: 0,
+        transition: {
+            duration: 1,
+        }
+    })
 }
 
 export default function App(){
-    const [isShow, setIsShow] = useState(false);
-    const toggleShow = () => setIsShow((prev) => !prev)
-    
+    const [visible, setVisible] = useState(1);
+    const [isBack, setIsBack] = useState(false);
+
+    const backDic = " < ";
+    const nextDic = " > ";
+
+    const backBtn = () => {
+        setIsBack(true);
+        setVisible((prev) => (prev === 1 ? 1 : prev -= 1))
+    }
+    const nextBtn = () => {
+        setIsBack(false);
+        setVisible((prev) => (prev === 10 ? 10 : prev += 1))
+    }
+
     return (
         <Wrapper>
-            <button onClick={toggleShow}>Click</button>
-
-            {/* 밖에서 선언한다. */}
-            <AnimatePresence> 
-                {isShow ? (
-                    <Box 
-                        variants={boxVariants}
-                        initial="initial"
-                        animate="visible"
-                        exit="leaving"
-                    /> 
-                ) : null}
+            <AnimatePresence custom={isBack}> 
+                <Box 
+                    custom={isBack} // 버튼에 따라 애니메이션 흐름을 다르게 해준다.
+                    variants={box}
+                    initial="entry"
+                    animate="center"
+                    exit="exit"
+                    key={visible}
+                >
+                    {visible}
+                </Box> 
             </AnimatePresence>
+            <BtnArea>
+                <button onClick={backBtn}>{backDic}</button>
+                <button onClick={nextBtn}>{nextDic}</button>
+            </BtnArea>
         </Wrapper>
     );
 }
