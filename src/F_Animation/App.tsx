beda678844ef56dc684bbc37fa98e29993dcc3c7
krille <mks160615@gmail.com>
2026-08-05 20:04:26 +0900

    layout hard

--- src/F_Animation/App.tsx
+++ src/F_Animation/App.tsx
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import { motion } from "framer-motion";
+import { motion, AnimatePresence } from "framer-motion";
 import { useState } from "react";
 
 const Wrapper = styled.div`
@@ -10,53 +10,66 @@ const Wrapper = styled.div`
     align-items: center;
 `;
 
-const Box = styled(motion.div)`
-    width: 200px;
-    height: 200px;
-    background-color: rgba(255,255,255,1);
-    border-radius: 40px;
-    display: flex;
-    justify-content:center;
-    align-items: center;
-    box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+const Grid = styled.div`
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    width: 50vw;
+    gap: 10;
+
+    div: first-child,
+    div: last-child {
+        grid-column: span 2;
+    }
 `;
 
-const Circle =styled(motion.div)`
-    background-color: rgba(0, 165, 255, 1);
-    height: 40px;
-    width: 40px;
-    border-radius: 100%;
-    box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+const Box = styled(motion.div)`
+    background-color: rgba(255,255,255,1);
+    border-radius: 40px;
+    height: 100px;
+    justify-content:center;
+    align-items: center;
+`;
+
+const Overlay = styled(motion.div)`
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    display: flex;
+    justify-content: center;
+    align-items: center;
 `
 
 export default function App(){
-    const [isClick, setIsClick] = useState(false);
-    const toggleClick = () => {
-        setIsClick((prev)=> !prev);
-    }
+    const [clickedBox, setClickedBox] = useState<null|number>(null);
 
     return (
-        // layout: 순간 이동이 아닌 자연스러운 이동을 보여줌
-        // layoutId: wrapper 내부에 깍쇄간 이동 가능
-        <Wrapper onClick={toggleClick}>
-            <Box>
-                {!isClick ? 
-                    <Circle 
-                        layoutId="circle"
-                        style={{borderRadius: 50}}
-                    /> 
-                    : null
+        <Wrapper>
+            <Grid>
+                {[1,2,3,4].map(
+                    (n) => (
+                        <Box 
+                            onClick={()=>setClickedBox(n)}
+                            key={n} 
+                            layoutId={n+""}
+                        />
+                    ))}
+            </Grid>
+            <AnimatePresence>
+                {clickedBox ? (
+                    <Overlay 
+                        onClick={()=>setClickedBox(null)}
+                        initial={{backgroundColor: "rgba(0,0,0,0)"}}
+                        animate={{backgroundColor: "rgba(0,0,0,0.5)"}}
+                        exit={{backgroundColor: "rgba(0,0,0,0)"}}
+                    >
+                        <Box 
+                            layoutId = {clickedBox+""}
+                            style={{width: 200, height: 100}}
+                        />
+                    </Overlay>
+                    ) : null
                 }
-            </Box>
-            <Box>
-                {isClick ? 
-                    <Circle 
-                        layoutId="circle"
-                        style={{borderRadius: 0, scale: 2}}
-                    /> 
-                    : null
-                }
-            </Box>  
+            </AnimatePresence>
         </Wrapper>
     );
 }
